09ffe81914884a72d5ca1fdb8499c8315e62a86b
Artem23112 <artemtchernichuk@yandex.ru>
2023-12-30 18:02:42 +0200

    took out extra info from component

--- src/components/layout/doctor-workspace/DoctorWorkspace.tsx
+++ src/components/layout/doctor-workspace/DoctorWorkspace.tsx
@@ -1,15 +1,16 @@
-import moment from 'moment'
-import { useState } from 'react'
-import s from './DoctorWorkspace.module.scss'
-import { UniqueDoctorAppointmentT } from '@/redux/slices/appointments-slice/additionalThunks/serverDoctorCommunication/types'
-import { SortItemConfigT, SortButtons } from '@/components/UI/sort-buttons/SortButtons'
+import { SortButtons } from '@/components/UI/sort-buttons/SortButtons'
 import {
 	StyledCalendar,
 	valuePiece,
 } from '@/components/UI/styled-calendar/StyledCalendar'
 import { useFilterAppointments } from '@/hooks/useFilterAppointments'
+import { UniqueDoctorAppointmentT } from '@/redux/slices/appointments-slice/additionalThunks/serverDoctorCommunication/types'
 import { StatusAppointmentT } from '@/redux/slices/appointments-slice/types'
+import moment from 'moment'
+import { useState } from 'react'
 import { PatientList } from '../patient-list/PatientList'
+import s from './DoctorWorkspace.module.scss'
+import { sortConfig } from './sortConfig'
 
 type DoctorWorkspacePropsT = {
 	doctorAppointments: UniqueDoctorAppointmentT[]
@@ -23,26 +24,6 @@ export const DoctorWorkspace = ({ doctorAppointments }: DoctorWorkspacePropsT) =
 		UniqueDoctorAppointmentT,
 		FiltersT
 	>(doctorAppointments, 'all', chosenDate)
-
-	const sortConfig: SortItemConfigT[] = [
-		{
-			filter: 'all',
-			btnText: 'Все',
-		},
-		{
-			filter: 'enrolled',
-			btnText: 'Записанные',
-		},
-		{
-			filter: 'admitted',
-			btnText: 'Принятые',
-		},
-		{
-			filter: 'not-admitted',
-			btnText: 'Не принятые',
-		},
-		{ filter: 'expired', btnText: 'Просроченные' },
-	]
 
 	return (
 		<>
