a29dd5d9f56e33b4c0eb54829783f2b24201f24e
Artem23112 <artemchernichuk@gmail.com>
2025-11-04 18:04:21 +0200

    Commented separating on chunks

--- vite.config.ts
+++ vite.config.ts
@@ -12,17 +12,17 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
+      // output: {
+      //   manualChunks(id) {
+      //     if (id.includes("node_modules")) {
+      //       return id
+      //         .toString()
+      //         .split("node_modules/")[1]
+      //         .split("/")[0]
+      //         .toString();
+      //     }
+      //   },
+      // },
     },
     chunkSizeWarningLimit: 1000, // Устанавливаем лимит в 1000 кБ
   },
