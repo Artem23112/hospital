96fc5dc60c3a09c5c99882d1fec230bde9d50aaf
Artem23112 <artemchernichuk@gmail.com>
2025-11-04 15:51:40 +0200

    Fixed issues with import paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./hospital/",
+  base: "/hospital/",
   plugins: [react()],
   resolve: {
     alias: {
