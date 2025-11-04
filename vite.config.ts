04a21dc95da19cd7a12072f34c48e82b5533c0d3
Artem23112 <artemchernichuk@gmail.com>
2025-11-04 15:42:11 +0200

    changed base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./hospital/",
   plugins: [react()],
   resolve: {
     alias: {
